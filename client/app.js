fe40c897b2ffcbdaaa9fd0d4eca4b6031737ae0d
Jonathan Krone <jkrone@vt.edu>
2016-03-10 11:33:04 -0600

    (feat) Add middleware to browserHistory so that we can redux to dispatch changes in history and location. Also move initial state to its own file

--- client/app.js
+++ client/app.js
@@ -6,7 +6,7 @@ import { createStore } from 'redux';
 
 import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { routerReducer } from './reducers/routerReducer';
+import { routeReducer } from './reducers/routeReducer';
 
 import { fromJS } from 'immutable';
 import { combineReducers } from 'redux-immutable';
@@ -16,55 +16,7 @@ import { ProfileContainer } from './containers/Profile';
 import { MainContainer } from './containers/Main';
 import rootReducer from './reducers/rootReducer';
 
-const dummyState = {
-  user: {
-    id: 1,
-    avatar: 'https://i.ytimg.com/vi/1v6M41Divso/maxresdefault.jpg',
-    fullName: 'Kim Panda',
-    street: 'back handle lane',
-    city: 'Austin',
-    state: 'Virginia',
-    zip: '56352',
-  },
-  friends: [
-    {
-      id: 2,
-      avatar: 'http://spirit-animals.com/wp-content/uploads/2012/09/Dolphin.jpg',
-      fullName: 'Flipp',
-    },
-    {
-      id: 7,
-      avatar: 'http://vignette3.wikia.nocookie.net/pokemon/images/1/' +
-              '16/025Pikachu_OS_anime_10.png/revision/20150102074354',
-      fullName: 'PIKA',
-    },
-  ],
-  profileInfoItems: [
-    { itemTitle: 'Austin' },
-    { itemDesc: 'Virginia' },
-  ],
-  drivers: [
-    {
-      id: 10,
-      avatar: 'http://animaliaz-life.com/data_images/dog/dog7.jpg',
-      fullName: 'Barkdog',
-    },
-    {
-      id: 15,
-      avatar: 'http://media4.popsugar-assets.com/files/2014/08/08/878/n/1922507' +
-      '/caef16ec354ca23b_thumb_temp_cover_file32304521407524949.xxxlarge/i/Funny-Cat-GIFs.jpg',
-      fullName: 'Mrowr',
-    },
-  ],
-  isDriver: false,
-  isRider: false,
-  isMatched: false,
-  isConfirmed: false,
-  isWaitingForMatch: false,
-  match: null,
-  displaySettings: false,
-  displayMatchInfo: false,
-};
+import InitialState from './initialState';
 
 // Update /profile to /profile/:userId when ready. react-router.
 const routes = <Route component={Root}>
@@ -74,11 +26,21 @@ const routes = <Route component={Root}>
 
 const reducersWithRouter = combineReducers({
   rootReducer,
-  routing: routerReducer,
+  routing: routeReducer,
 });
 
-const store = createStore(reducersWithRouter, fromJS(dummyState));
-const history = syncHistoryWithStore(browserHistory, store);
+const historyMiddleware = routerMiddleware(browserHistory);
+const store = createStore(
+  reducersWithRouter,
+  applyMiddleware(historyMiddleware),
+  fromJS(InitialState)
+);
+
+const history = syncHistoryWithStore(browserHistory, store, {
+  selectLocationState(state) {
+    return state.getIn('locationBeforeTransitions').toJS();
+  },
+});
 
 ReactDOM.render(
   <Provider store={store}>
